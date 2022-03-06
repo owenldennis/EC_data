dbf60bc6f947b8948a36f7a53229209efd4e3587
owen dennis <owenldennis@hotmail.com>
2022-03-06 17:35:47 +0000

    data_extraction_working

---
+++ src/data_extraction.py
@@ -0,0 +1,210 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Pupil data for Eastbourne College project
+This file contains functions to set up a dataframe containing all relevant information.
+Cross referencing of multiple csv files in order to extract the names of pupils and courses based on numerical codes
+
+"""
+
+import pandas as pd
+import csv
+import matplotlib.pyplot as plt
+import random 
+import numpy as np
+import openpyxl
+from sklearn.linear_model import LinearRegression
+
+# source data directory from onedrive
+SOURCE_DATA_DIR = "C:/Users/owen/OneDrive - Eastbourne College/School analytics project"
+
+# MidYIS excel files are imported as multi-level column dataframes.
+# default constants for selecting data
+TUPLES = [('MidYIS', 'Overall Score'), ('Pupil Information', 'Surname')]
+SUBJECT = 'Mathematics'
+CRITERIA = 'Predicted GCSE Points'# options include 'Predicted GCSE Points'
+TUPLES.append((SUBJECT, CRITERIA))
+
+
+
+
+# Decide which data to analyse
+USE_NUMERICAL_GRADING_DATA = False
+USE_OLD_GRADING_DATA = True
+REMOVE_CAGS_TAGS = False
+#print(TUPLES)
+
+"""
+list of excel sheet titles (keys for extracted dictionary)
+['Yr 9 2012_13', 'Yr 9 2013_14',  ***all subjects graded 1-8 on CEM scale***
+ 'Yr 9 2014_15', 'Yr 9 2014_15 (9-1)', ***all except English graded 1-8)***
+'Yr 9 2015_16', 'Yr 9 2015_16 (9-1)', ***just a few pupils in maths, history and DT graded 1-8 still*** 
+'Yr 9 2016_17 (9-1)', 'Yr 9 2017_18 (9-1)', 'Yr 9 2018_19 (9-1)'  ***all subjects graded 1-9***  
+]
+"""
+SHEET_TITLES_OLD = ['Yr 9 2012_13', 'Yr 9 2013_14', 'Yr 9 2014_15', 'Yr 9 2015_16']
+if REMOVE_CAGS_TAGS:
+    SHEET_TITLES_NEW = ['Yr 9 2014_15 (9-1)', 'Yr 9 2015_16 (9-1)','Yr 9 2016_17 (9-1)']
+else:
+    SHEET_TITLES_NEW =  ['Yr 9 2014_15 (9-1)', 'Yr 9 2015_16 (9-1)','Yr 9 2016_17 (9-1)', 
+                     'Yr 9 2017_18 (9-1)', 'Yr 9 2018_19 (9-1)']
+
+SHEETS = SHEET_TITLES_OLD
+if USE_NUMERICAL_GRADING_DATA:
+    if USE_OLD_GRADING_DATA:
+        SHEETS = SHEET_TITLES_OLD + SHEET_TITLES_NEW
+    else:
+        SHEETS = SHEET_TITLES_NEW
+    
+# load all excel sheets from 2012 to 2018
+midyis_and_GCSE_data = pd.read_excel(SOURCE_DATA_DIR + "/MidYIS_average_and_GCSE_scores.xlsm",
+                                 sheetname = None,
+                            header = [0,1])
+
+
+def select_frames(dict_of_dfs = midyis_and_GCSE_data, keys = SHEET_TITLES_OLD,
+                  subject = 'Mathematics', criteria = 'Actual GCSE Points'):   
+    valid_sheets = []
+    for key in keys:
+        try:
+            midyis_and_GCSE_data[key][TUPLES]
+            valid_sheets.append(key)
+        except KeyError:
+            pass
+    return valid_sheets
+
+def clean_data(data, verbose = False, remove_any_non_numeric_grades = True):
+    raw_length = len(data.index)
+    data = data.dropna()
+    nan_removed_length = len(data.index)
+    #data = data[data[(SUBJECT, CRITERIA)] != 'U']
+    #U_removed_length = len(data.index)
+    
+    if remove_any_non_numeric_grades:
+        data = data[data[(SUBJECT, CRITERIA)].isin(range(1,10))]#['1','2','3','4','5','6','7','8','9'])]
+        only_numeric_length = len(data.index)
+    else:
+        only_numeric_length = "Not done"
+    
+    if verbose:
+        summary_dict = {"Initial dataset length" : [raw_length],
+                                        "After removing NaN entries" : [nan_removed_length],
+                                        #"After removing U grades" : [U_removed_length],
+                                        "After removing other non-numeric grades" : [only_numeric_length],
+                                        }
+        display(pd.DataFrame.from_dict(summary_dict, orient = 'index').rename({0 : "Length"}))#, columns = ['Length']))
+        print("\n")
+    return data
+    
+# pull out specific columns for a dataframe (relates to constants above) and concatenate
+valid_sheets = select_frames(keys = SHEETS, subject = SUBJECT, criteria = CRITERIA)
+print("The valid sheet titles for this analysis are :{0}\n".format(valid_sheets))
+data = pd.concat([midyis_and_GCSE_data[sheet][TUPLES] for sheet in valid_sheets],
+                  ignore_index = True)
+
+data = clean_data(data, remove_any_non_numeric_grades = False, verbose = True)
+
+
+#display(data.head(10))
+#print(data.columns)
+plt.scatter(data[('MidYIS', 'Overall Score')], data[SUBJECT, CRITERIA])
+X = data[('MidYIS', 'Overall Score')].values.reshape(-1,1)
+y = data[SUBJECT, CRITERIA].values
+linreg = LinearRegression().fit(X, y)
+
+print("Regression gives R^2 score of {0:.2f}".format(linreg.score(X, y)))
+
+
+
+
+"""
+Code below is used for collating information about EC pupils from the spreadsheets 
+"""
+
+# rename columns for midyis data
+#midyis_2021.rename(columns = {'Unnamed: 6' : 'Score', 'Unnamed: 7' : 'Band'})
+# make cross reference dictionary for passed columns (must be aligned)
+def make_crossref_dict(df, keys_col_heading, vals_col_heading):
+    keys_col = df[keys_col_heading]
+    vals_col = df[vals_col_heading]
+    return {key:vals_col[i] for i, key in enumerate(keys_col)}
+ 
+# replace whitespace with '_' in all headings in dataframe
+def remove_whitespace_in_col_headings(df):
+    col_dict = {col : col.replace(' ', '_') for col in df.columns}
+    df.rename(columns = col_dict, inplace = True)
+
+# cross reference between two dataframes and append cross-referenced data to the first one passed in
+def lookup_and_append_to_df(df_to_append, lookup_col_heading, new_col_heading,
+                            df_source, keys_col_heading, values_col_heading, 
+                            display_sample = False, verbose = False):
+
+    d = make_crossref_dict(df_source, keys_col_heading, values_col_heading)
+    missing_keys = [k for k in df_to_append[lookup_col_heading] if not k in d.keys()]
+    for key in missing_keys:
+        d[key] = 'Missing data'
+    df_to_append[new_col_heading] = df_to_append[lookup_col_heading].map(lambda x : d[x])
+    
+    if display_sample:
+        print("Random sample from appended dataframe:")
+        size = 10
+        random_indices = np.random.randint(len(df_to_append.index), size = size)
+        sampled_df = df_to_append[[lookup_col_heading, new_col_heading]].loc[random_indices, :].sort_values(by = lookup_col_heading)
+        display(sampled_df)
+        keys = df_to_append[lookup_col_heading].loc[random_indices]
+        #print("Keys are: {0}".format(keys))
+        df_mask = df_source.where(df_source[keys_col_heading].isin(keys))
+        print("Matching rows from source dataframe")
+        df_to_display = df_mask[[keys_col_heading, values_col_heading]].dropna().sort_values(by = keys_col_heading)
+        display(df_to_display)
+    
+    if verbose:
+        print("There were {0} unique entries in the lookup dataframe which could not be found in the source dataframe".format(len(set(missing_keys))))
+        print("The new column contains {0} 'Missing data' entries".
+              format(len(df_to_append[df_to_append[new_col_heading] == 'Missing data'].index)))
+        
+        lookup_series = df_to_append[lookup_col_heading]
+        print("Lookup column contained {0} entries, of which {2} were NaN. There were {1} unique entries.".
+             format(len(lookup_series.index), len(lookup_series.value_counts().index), lookup_series.isnull().sum()))
+
+def collate_pupil_data_from_EC_files():  
+    # import relevant data
+    results_dataframe = pd.read_csv(SOURCE_DATA_DIR + "/ECi_External Exams_csv_results.csv")
+    programmes_subjects_df = pd.read_csv(SOURCE_DATA_DIR + "/ECi_External Exams_csv_programmes.csv")
+    pupil_info_df = pd.read_csv(SOURCE_DATA_DIR + "/ECi_Pupil Data_pupils.csv")     
+    # Remove whitespace from column headings   
+    remove_whitespace_in_col_headings(results_dataframe)
+    remove_whitespace_in_col_headings(programmes_subjects_df)
+    remove_whitespace_in_col_headings(pupil_info_df)
+    
+    # cross reference and append data to results_dataframe
+    
+    # append exam course names
+    lookup_and_append_to_df(results_dataframe, 'Programme_Id', 'Programme_Name',
+                            programmes_subjects_df, 'Programme_Id', 'Name',
+                            display_sample = False, verbose = False)
+    # append pupil initials
+    lookup_and_append_to_df(results_dataframe, 'Pupil_Id', 'Initials',
+                            pupil_info_df, 'Id', 'Name_Initials',
+                            display_sample = False, verbose = False)
+    # append pupil surnames
+    lookup_and_append_to_df(results_dataframe, 'Pupil_Id', 'Surname',
+                            pupil_info_df, 'Id', 'Surname',
+                            display_sample = False, verbose = False)
+    # append pupil forenames
+    lookup_and_append_to_df(results_dataframe, 'Pupil_Id', 'Forename',
+                            pupil_info_df, 'Id', 'Forename',
+                            display_sample = False, verbose = False)
+    # append pupil date of birth
+    lookup_and_append_to_df(results_dataframe, 'Pupil_Id', 'DOB',
+                            pupil_info_df,'Id', 'DOB',
+                            display_sample = False, verbose = False)
+
+
+
+
+
+
+
+#print(results_dataframe[['Programme_Id', 'Programme_Name', 'First_Grade', 'Initials']].head(10))
---
+++ src/unused_funcs.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 24 18:33:42 2022
+obsolete functions
+
+@author: owen
+"""
+
+def test_with_mini_df():
+    mini_df = results_dataframe[['Pupil Id', 'Programme Id']].head(10)   
+    display(mini_df)
+    print(mini_df.columns)
+    remove_whitespace_in_col_headings(mini_df)
+    remove_whitespace_in_col_headings(programmes_subjects_df)    
+    print(mini_df.columns)
+    mini_df['Programme Name'] = mini_df['Programme_Id'].map(lambda x : get_prog_name(x))
+    display(mini_df)
+
+def get_prog_name(prog_id):
+    for row in programmes_subjects_df.itertuples():
+        if row.Programme_Id == prog_id:
+            return row.Name
+    return np.nan
+
+def new_col_from_crossref(col_to_cross_ref, df_to_read, keys_col_heading, vals_col_heading):
+    crossref_dict = make_crossref_dict(df_to_read, keys_col_heading, vals_col_heading)
+    return [crossref_dict[col_entry] for col_entry in col_to_cross_ref]
+
+# create cross-ref dictionaries
+#codes_to_subjects = make_crossref_dict(programmes_subjects_df, 'Programme_Id', 'Name') 
+#ids_to_initials = make_crossref_dict(pupil_info_df, 'Id', 'Name_Initials')
+#print(results_dataframe.columns)
+#print(pupil_info_df.columns)
+#print(results_dataframe[['Programme_Id', 'First_Grade']].head(10))
+
+# add new columns to dataframe using cross-references
+#results_dataframe['Programme_name'] = results_dataframe['Programme_Id'].map(lambda x : codes_to_subjects[x])
+#results_dataframe['Initials'] = results_dataframe['Pupil_Id'].map(lambda x : ids_to_initials[x])
+#display(results_dataframe[['Pupil_Id', 'Programme_Id', 'Programme_name', 'First_Grade']].head(20))
