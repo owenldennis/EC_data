3e46993712c487c4b3c49f553f5b013dbaffb06d
owen dennis <owenldennis@hotmail.com>
2022-08-25 13:07:50 +0100

    pivot_good_merge_bad

--- src/Combine ALIS with GCSE data.py
+++ src/Combine ALIS with GCSE data.py
@@ -12,6 +12,7 @@ Created on Wed Aug 17 16:52:02 2022
 import ALIS_data_extraction as ALIS_ext
 import data_extraction as de
 import pandas as pd
+import numpy as np
 
 ALL_ALIS_FILE = "{0}/all_years.csv".format(ALIS_ext.ALIS_DATA_DIR)
 
@@ -20,63 +21,111 @@ YEAR_MATCH = {'Yr 9 2012_13' : '2017.csv',
               'Yr 9 2013_14' : '2018.csv',
               'Yr 9 2014_15' : '2019.csv',
               'Yr 9 2015_16' : '2020.csv',
-              'Yr 9 2016_17' : '2021.csv'}
+              'Yr 9 2016_17 (9-1)' : '2021.csv'}
 
 midYIS_data_years = de.ALL_YEARS
 
-def merge_results_wrapper(year, MidYIS_file = de.MidYIS_GCSE_DATA_FILE):
+def pivot_dataframe(df, pivot_index = ['Surname', 'Forename'], 
+                    pivot_columns = 'Subject Title ', 
+                    pivot_values = 'A level Grade', 
+                    columns_to_drop = ["Subject Title ", 'A level Grade','Exam Type',  'A level Points',
+                                         'Syllabus Title', 'Exam Board', 'Syllabus Code'],
+                    check_for_errors = True, verbose = False):
+    
+    # pivots passed dataframe according to the parameters passed
+    # checks for consistency with original if check_for_errors is True
+    
+    #df = pd.read_csv("{0}/{1}".format(ALIS_ext.ALIS_DATA_DIR, '2017.csv'))
+    if check_for_errors:
+        df_ref = df.copy()
+
+    
+    # pivot
+    new_df = df.pivot(index = pivot_index, columns = pivot_columns, values = pivot_values)
+    new_df.reset_index(inplace = True)
+       
+    # drop columns that are now irrelevant from original df
+    df.drop(columns = columns_to_drop, inplace = True)
+    
+    # join back to original dataframe with duplicates removed
+    new_df = new_df.join(other = 
+                         df.drop_duplicates(subset = pivot_index).set_index(pivot_index), 
+                                            on = pivot_index, how = 'left')
+    
+    # test to confirm that grades are correctly allocated
+    if check_for_errors:
+        for row_index in df_ref.index:
+            surname = df_ref.loc[row_index,['Surname']]['Surname']
+            forename = df_ref.loc[row_index,['Forename']]['Forename']
+            subject_title = df_ref.loc[row_index, ['Subject Title ']]['Subject Title ']
+            grade = df_ref.loc[row_index, ['A level Grade']]['A level Grade']
+            
+            matching = new_df[new_df['Surname'] == surname]
+            matching = matching[matching.Forename == forename]
+            matching = matching[subject_title].copy()
+            #matching.dropna(inplace = True)
+            try:
+                assert(len(matching.index) == 1)
+            except AssertionError:
+                print("Multiple matches for name {0} and subject {1}.".format(forename + " " + surname, subject_title))
+                print("Dataframe of matching entries is {0}\n".format(matching))
+                    
+            try:
+                assert(matching.iloc[0] == grade)
+            except AssertionError:
+                print("Mismatch between grade stored in original dataframe and pivoted dataframe.")
+                print("Row of original dataframe for {1} is \n{0}\n".format(forename + " " + surname + " " + subject_title,
+                                                                            df_ref.loc[row_index]))
+            if verbose:
+                print("Confirming pivot operation for {0} {1} {2}".format(forename, surname, subject_title))
+                print("Grade stored in pivoted dataframe : {0}".format(matching.iloc[0]))
+                print("Grade stored in original dataframe : {0}".format(grade))
+    
+        new_df.to_csv("{0}/test_join.csv".format(de.TEMP_DIR))
+    
+    return new_df
+
+
+def merge_GCSE_and_ALIS_results(year, MidYIS_file = de.MidYIS_GCSE_DATA_FILE,
+                                remove_AS_results = True, use_points = True, verbose = False):
     
     dfs = {}
-    dfs["MidYIS data for " + year] = de.get_columns_and_flatten(year)
-    dfs["ALIS data for " + YEAR_MATCH[year]] = pd.read_csv("{0}/{1}".format(ALIS_ext.ALIS_DATA_DIR, YEAR_MATCH[year]))
     
-    print(dfs["MidYIS data for " + year]['Surname'].sort_values())
-    print(dfs["ALIS data for " + YEAR_MATCH[year]]['Surname'].sort_values())
+    # read in MidYIS and GCSE data for year passed
+    # check if the year is one where numeric and alpha grades need to be combined into one dataframe
+    if year in ['Yr 9 2014_15', 'Yr 9 2015_16']:
+        dfs["MidYIS data for " + year] = de.merge_years_wrapper(years = [year, year +' (9-1)'], test_run = False)
+    else:
+        dfs["MidYIS data for " + year] = de.get_columns_and_flatten(year)
+    
+    # read in ALIS data for corresponding year
+    ALIS_df = pd.read_csv("{0}/{1}".format(ALIS_ext.ALIS_DATA_DIR, YEAR_MATCH[year]))
+    
+    # remove AS results if required
+    if remove_AS_results:
+        ALIS_df = ALIS_df[ALIS_df['Exam Type'] == 'A-Level (A2)']
+        if verbose:
+            print("AS results removed from ALIS datafile for year {0}".format(YEAR_MATCH[year]))
+    
+    # pivot ALIS data so subjects are column headings with grade/point values
+    dfs["ALIS data for " + YEAR_MATCH[year]] = pivot_dataframe(ALIS_df,
+                                                    pivot_index = ['Surname', 'Forename'], 
+                                                    pivot_columns = 'Subject Title ', 
+                                                    pivot_values = 'A level Grade', 
+                                                    columns_to_drop = ["Subject Title ", 'A level Grade','Exam Type',  'A level Points',
+                                                                         'Syllabus Title', 'Exam Board', 'Syllabus Code'],
+                                                    check_for_errors = True, verbose = verbose)
+                                
+   # print(dfs["MidYIS data for " + year]['Surname'].sort_values())
+    #print(dfs["ALIS data for " + YEAR_MATCH[year]]['Surname'].sort_values())
     #print(dfs["ALIS data"].head())
     
-    de.merge_wrapper(dfs, on = 'Surname', test_run = True)
-
-
-#merge_results_wrapper('Yr 9 2014_15')
-
-df = pd.read_csv("{0}/{1}".format(ALIS_ext.ALIS_DATA_DIR, '2017.csv'))
-df_A_ref = df[df['Exam Type'] == "A-Level (A2)"]
-#drop AS results
-df_A = df[df['Exam Type'] == "A-Level (A2)"].copy()
-
-# pivot to create subject title columns with grades as values
-new_df = df_A.pivot(index = ['Surname', 'Forename'], columns = 'Subject Title ', values = 'A level Grade')
-new_df.reset_index(inplace = True)
-verbose = False
-
-
-
-
-# drop columns that are now irrelevant from df_A
-df_A.drop(columns = ["Subject Title ", 'A level Grade','Exam Type',  'A level Points',
-                     'Syllabus Title', 'Exam Board', 'Syllabus Code'], inplace = True)
-
-# join back to original dataframe with duplicates removed
-new_df = new_df.join(other = df_A.drop_duplicates(subset = ['Surname', 'Forename']).set_index(['Surname', 'Forename']), 
-                                        on = ['Surname', 'Forename'], how = 'left')
-
-# test to confirm that grades are correctly allocated
-for row_index in df_A_ref.index:
-    surname = df_A_ref.loc[row_index,['Surname']]['Surname']
-    forename = df_A_ref.loc[row_index,['Forename']]['Forename']
-    subject_title = df_A_ref.loc[row_index, ['Subject Title ']]['Subject Title ']
-    grade = df_A_ref.loc[row_index, ['A level Grade']]['A level Grade']
+    merged_data = de.merge_wrapper(dfs, on = 'Surname', test_run = True)
     
-    matching = new_df[new_df['Surname'] == surname]
-    matching[matching.Forename == forename]
-    matching = matching[subject_title]
-    assert(len(matching.index) == 1)
-    assert(matching.iloc[0] == grade)
-    #print(matching)
-    if verbose:
-        print("Confirming pivot operation for {0} {1} {2}".format(forename, surname, subject_title))
-        print("Grade stored in pivoted dataframe : {0}".format(matching.iloc[0]))
-        print("Grade stored in original dataframe : {0}".format(grade))
+    merged_data.to_csv("{0}/merged_MidYIS_ALIS.csv".format(de.TEMP_DIR))
+    
+    return merged_data
 
-new_df.to_csv("{0}/test_join.csv".format(de.TEMP_DIR))
-
+if __name__ == '__main__':
+    year = list(YEAR_MATCH.keys())[1]
+    merge_GCSE_and_ALIS_results(year, verbose = False)
